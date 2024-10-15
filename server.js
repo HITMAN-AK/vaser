bb4e9fb100918009411c21f10aaa707d87d43e22
Nithin-3 <legendaryairforce@gmail.com>
2024-10-15 17:28:14 +0530

    init

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const app =express();
 const cors = require("cors");
 const bodyparser=require("body-parser");
 const mongoose = require("mongoose");
+const password = encodeURIComponent("Ashwin@01012004");
 app.use(bodyparser.json());
 app.use(
     cors({
       origin: "*",
     })
 );
+main().catch((err) => {
+  console.log(err);
+});
+async function main() {
+  await mongoose
+    .connect(
+      `mongodb+srv://legendaryairforce:${password}@mrdb.expdex1.mongodb.net/mrdb`
+    )
+    .then(() => {
+      console.log("db connect success");
+    })
+    .catch((err) => {
+      console.log("db connect error", err);
+    });
+}
+app.listen(2000, "0.0.0.0", () => {
+  console.log(`Server running on port`);
+});
